dc3722e4e142b49f96d8424ba0b29f990f2d71ea
Saulo CdeF <saulocdef@gmail.com>
2025-10-05 20:43:27 -0300

    adiciona botões e centraliza conteúdo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,30 +6,30 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div style={{ width: 250 }}>
           <Image
-        className="light:invert"
-        src="/logo.png"
-        alt="FAHRENHurb Logo"
-        width={250}
-        height={0}
-        style={{ height: "auto", width: "100%" }}
-        priority
+            className="light:invert"
+            src="/logo.png"
+            alt="FAHRENHurb Logo"
+            width={250}
+            height={0}
+            style={{ height: "auto", width: "100%" }}
+            priority
           />
         </div>
         <div className="text-sm/6 text-center sm:text-left text-white mt-24">
-          <div className="tracking-[-.01em] text-3xl font-bold">
-        YOUR FIRST TIME{" "}
-          </div>
-          <div className="mt-2 tracking-[-.01em]">
-        CLICK HERE TO ENJOY{" "}
-          </div>
-          <div className="mt-6">
-        <a
-          href="/map"
-          className="rounded-full bg-green-600 text-white px-4 py-2 text-lg font-semibold shadow hover:bg-green-700 transition-colors inline-block text-center"
-        >
-          Start journey
-        </a>
-          </div>
+            <div className="tracking-[-.01em] text-3xl font-bold flex justify-center">
+            YOUR FIRST TIME{" "}
+            </div>
+            <div className="mt-2 tracking-[-.01em] flex justify-center">
+            CLICK HERE TO ENJOY{" "}
+            </div>
+            <div className="mt-6 flex justify-center">
+            <a
+              href="/map"
+              className="rounded-full bg-green-600 text-white px-4 py-2 text-lg font-semibold shadow hover:bg-green-700 transition-colors inline-block text-center"
+            >
+              Start journey
+            </a>
+            </div>
         </div>
       </main>
     </div>
